99978f8059178e9bb582bbf18f8dcf42b6589ca1
GOconnects <globalopportunityconnects@gmail.com>
2025-04-04 08:25:57 +0100

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA8742Bs5nzfmPd5PkkE4-G8oVJR-38Mrw",
+    authDomain: "engagement-portal-39346.firebaseapp.com",
+    projectId: "engagement-portal-39346",
+    storageBucket: "engagement-portal-39346.firebasestorage.app",
+    messagingSenderId: "119604507798",
+    appId: "1:119604507798:web:7201634d294fc1312bfeb7"
+  };
+
+ 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  const auth=getAuth();
+  const db=getFirestore();
+
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists()){
+                const userData=docSnap.data();
+                document.getElementById('loggedUserFName').innerText=userData.firstName;
+                document.getElementById('loggedUserEmail').innerText=userData.email;
+                document.getElementById('loggedUserLName').innerText=userData.lastName;
+
+            }
+            else{
+                console.log("no document found matching id")
+            }
+        })
+        .catch((error)=>{
+            console.log("Error getting document");
+        })
+    }
+    else{
+        console.log("User Id not Found in Local storage")
+    }
+  })
+
+  const logoutButton=document.getElementById('logout');
+
+  logoutButton.addEventListener('click',()=>{
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+    .then(()=>{
+        window.location.href='index.html';
+    })
+    .catch((error)=>{
+        console.error('Error Signing out:', error);
+    })
+  })
+  
